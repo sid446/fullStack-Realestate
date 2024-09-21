058639e82f35587afdcb44dfca397ce04da70b6f
sid446 <162982475+sid446@users.noreply.github.com>
2024-09-21 11:20:32 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("connected to MongoDB!")
+}).catch((err)=>{
+    console.log(err)
+})
 
 
 const app=express();
 
-app.listen(3000,()=>{
-    console.log('Server Running on port 3000')
+app.listen(process.env.PORT,()=>{
+    console.log(`Server Running on port ${process.env.PORT}`)
 })
