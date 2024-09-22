c5dbf1e710b77fd70e4bb1500fe2305966d76322
sid446 <162982475+sid446@users.noreply.github.com>
2024-09-22 10:52:31 +0530

    added test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -16,3 +17,5 @@ const app=express();
 app.listen(process.env.PORT,()=>{
     console.log(`Server Running on port ${process.env.PORT}`)
 })
+
+app.use("/api/user",userRouter)
