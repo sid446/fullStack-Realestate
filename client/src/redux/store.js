b11af023519f7a5aeed4d67a5a3eac2c628574fd
sid446 <162982475+sid446@users.noreply.github.com>
2024-09-26 14:30:29 +0530

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import {persistedReducer} from "redux-persist"
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+const rootReducer=combineReducers({user:userReducer})
+
+const PersistConfig={
+  key:'root',
+  storage,
+  version:1
+}
+const persistedReducer=persistedReducer(
+  PersistConfig,rootReducer
+)
 
 export const store = configureStore({
-  reducer: { 
-    user: userReducer 
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware({
       serializableCheck: false, // Disables serializability checks for actions
     }),
 });
+
+export const persistor=persistStore(store)
